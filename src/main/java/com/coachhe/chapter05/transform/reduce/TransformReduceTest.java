91b1db91c1dd611ff0c3b938d9e768d80b9d4a5f
coachhe <coachhe@git.code.oa.com>
2022-11-26 22:10:59 +0800

    add reduce transform

---
+++ src/main/java/com/coachhe/chapter05/transform/reduce/TransformReduceTest.java
@@ -0,0 +1,41 @@
+package com.coachhe.chapter05.transform.reduce;
+
+import com.coachhe.chapter05.Event;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @Author: CoachHe
+ * @Date: 2022/11/26 17:16
+ */
+public class TransformReduceTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataStreamSource<Event> stream = env.fromElements(new Event("Mary", "./home", 1000L),
+                new Event("Bob", "./cart", 2000L),
+                new Event("Alice", "./prod?id=100", 3000L),
+                new Event("Bob", "./prod?id=1", 3300L),
+                new Event("Bob", "./home", 3500L),
+                new Event("Alice", "./prod?id=200", 3200L),
+                new Event("Bob", "./prod?id=2", 3800L),
+                new Event("Bob", "./prod?id=3", 4200L)
+        );
+
+        // 1.统计每个用户的访问频次
+        stream.map(new MapFunction<Event, Tuple2<String, Long>>() {
+            @Override
+            public Tuple2<String, Long> map(Event event) throws Exception {
+                return Tuple2.of(event.user, 1L);
+            }
+        }).keyBy(data -> data.f0).reduce((ReduceFunction<Tuple2<String, Long>>) (stringLongTuple2, t1) -> null
+        );
+
+    }
+
+
+}
