a7596fad04c511b70b38e3bfb58734234925b2b1
coachhe <coachhe@tencent.com>
2022-11-26 22:40:39 +0800

    add reduce function

--- src/main/java/com/coachhe/chapter05/transform/reduce/TransformReduceTest.java
+++ src/main/java/com/coachhe/chapter05/transform/reduce/TransformReduceTest.java
@@ -5,6 +5,7 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -20,21 +21,31 @@ public class TransformReduceTest {
                 new Event("Bob", "./cart", 2000L),
                 new Event("Alice", "./prod?id=100", 3000L),
                 new Event("Bob", "./prod?id=1", 3300L),
-                new Event("Bob", "./home", 3500L),
                 new Event("Alice", "./prod?id=200", 3200L),
+                new Event("Bob", "./home", 3500L),
                 new Event("Bob", "./prod?id=2", 3800L),
                 new Event("Bob", "./prod?id=3", 4200L)
         );
 
         // 1.统计每个用户的访问频次
-        stream.map(new MapFunction<Event, Tuple2<String, Long>>() {
-            @Override
-            public Tuple2<String, Long> map(Event event) throws Exception {
-                return Tuple2.of(event.user, 1L);
-            }
-        }).keyBy(data -> data.f0).reduce((ReduceFunction<Tuple2<String, Long>>) (stringLongTuple2, t1) -> null
-        );
+        SingleOutputStreamOperator<Tuple2<String, Long>> clickByUser = stream.map(new MapFunction<Event, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> map(Event event) throws Exception {
+                        return Tuple2.of(event.user, 1L);
+                    }
+                })
+                .keyBy(data -> data.f0)
+                .reduce((ReduceFunction<Tuple2<String, Long>>) (value1, value2) -> Tuple2.of(value1.f0, value1.f1 + value2.f1)
+                );
 
+        // 2. 获取当前最活跃的用户
+        SingleOutputStreamOperator<Tuple2<String, Long>> result = clickByUser
+                .keyBy(data -> "key")
+                .reduce((ReduceFunction<Tuple2<String, Long>>) (value1, value2) -> value1.f1 > value2.f1 ? value1 : value2);
+
+        result.print();
+
+        env.execute();
     }
 
 
